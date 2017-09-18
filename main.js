12dfd5b46fd97e3fb85588a744ab458379fe686e
Marta <mar.najdzionek@gmail.com>
2017-09-18 22:16:08 +0200

    Adding shopping list function

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 /**
  * Created by Marta on 9/18/2017.
  */
-var elements = document.getElementById("shopping-field");
-var addButton = document.getElementById("add-button");
+document.getElementById("add-button").onclick = function () {
+    var text = document.getElementById("shopping-field").value;
+    var li = document.createElement("li");
+    var textnode = document.createTextNode(text);
+    li.appendChild(textnode);
+    document.getElementById("list").appendChild(li);
+
+}
+
 
